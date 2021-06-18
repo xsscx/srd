4a3d23fc851202e6692764297b8a997949efd7d1
xss.cx <xss@xss.cx>
2021-06-18 17:23:20 -0400

    ADd SRDI
    ADD SRDI

---
+++ SecurityResearchTools_20C80/usr/local/share/security-research-device/example-cryptex/src/dropbear/localoptions.h
@@ -0,0 +1,5 @@
+#define DSS_PRIV_FILENAME "/tmp/dropbear_dss_host_key"
+#define RSA_PRIV_FILENAME "/tmp/dropbear_rsa_host_key"
+#define ECDSA_PRIV_FILENAME "/tmp/dropbear_ecdsa_host_key"
+#define ED25519_PRIV_FILENAME "/tmp/dropbear_ed25519_host_key"
+#define DROPBEAR_PIDFILE "/tmp/dropbear.pid"
---
+++ SecurityResearchTools_20C80/usr/local/share/security-research-device/example-cryptex/src/simple-server/README.md
@@ -0,0 +1,8 @@
+# simple-server
+
+This is an example of a service that binds to a port
+and runs under launchd.
+
+We also sign in some entitlements!
+
+
