f47e9f82a053f2c33ea96e38ab5f2f7a97d02ae5
Hoyt LLC Research <xss@xss.cx>
2021-07-01 08:27:12 -0400

    Create S3_6_c15_c1_5-read.c
    S3_6_c15_c1_5-read.c Add

---
+++ code/registers/S3_6_c15_c1_5-read.c
@@ -0,0 +1,30 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+
+void write_sprr(uint64_t v)
+{
+    __asm__ __volatile__("msr S3_6_c15_c1_5, %0\n"
+                         "isb sy\n" ::"r"(v)
+                         :);
+}
+
+uint64_t read_sprr(void)
+{
+    uint64_t v;
+    __asm__ __volatile__("isb sy\n"
+                         "mrs %0, S3_6_c15_c1_5\n"
+                         : "=r"(v)::"memory");
+    return v;
+}
+
+
+int main(int argc, char *argv[])
+{
+    for (int i = 0; i < 64; ++i) {
+
+        printf("bit %02d: %016llx\n", i, read_sprr());
+    }
+}
