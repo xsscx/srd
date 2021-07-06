8f91547fda4b59a66c7a7f6703cf65bc5d92ae39
xss.cx <xss@xss.cx>
2021-07-06 09:48:11 -0400

    FUZZ s3_5_c15_c10 0-7
    FUZZ s3_5_c15_c10 0-7
    
    All SIGILL

---
+++ code/registers/s3_5_c15_c10_7/s3_5_c15_c10_7-read.c
@@ -0,0 +1,30 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+
+void write_sprr(uint64_t v)
+{
+    __asm__ __volatile__("msr s3_5_c15_c10_7, %0\n"
+                         "isb sy\n" ::"r"(v)
+                         :);
+}
+
+uint64_t read_sprr(void)
+{
+    uint64_t v;
+    __asm__ __volatile__("isb sy\n"
+                         "mrs %0, s3_5_c15_c10_7\n"
+                         : "=r"(v)::"memory");
+    return v;
+}
+
+
+int main(int argc, char *argv[])
+{
+    for (int i = 0; i < 64; ++i) {
+
+        printf("s3_5_c15_c10_7 bit %02d: %016llx\n", i, read_sprr());
+    }
+}
