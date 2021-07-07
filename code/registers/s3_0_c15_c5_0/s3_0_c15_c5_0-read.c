71e1187cf053fcb116c7176eda62d13aa3775051
xss.cx <xss@xss.cx>
2021-07-07 07:36:39 -0400

    add s3_0_c15_c5_0
    s3_0_c15_c5_0

---
+++ code/registers/s3_0_c15_c5_0/s3_0_c15_c5_0-read.c
@@ -0,0 +1,30 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+
+void write_sprr(uint64_t v)
+{
+    __asm__ __volatile__("msr s3_0_c15_c5_0, %0\n"
+                         "isb sy\n" ::"r"(v)
+                         :);
+}
+
+uint64_t read_sprr(void)
+{
+    uint64_t v;
+    __asm__ __volatile__("isb sy\n"
+                         "mrs %0, s3_0_c15_c5_0\n"
+                         : "=r"(v)::"memory");
+    return v;
+}
+
+
+int main(int argc, char *argv[])
+{
+    for (int i = 0; i < 64; ++i) {
+
+        printf("s3_0_c15_c5_0 bit %02d: %016llx\n", i, read_sprr());
+    }
+}
