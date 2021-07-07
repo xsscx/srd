d9cb7f17b21117c8ddf2794ac89553f09e1e0c81
Hoyt LLC Research <xss@xss.cx>
2021-07-06 21:54:10 -0400

    Update 0xfffffc118-read.c

--- code/registers/misc/0xfffffc118-read.c
+++ code/registers/misc/0xfffffc118-read.c
@@ -3,6 +3,8 @@
 
 int main(int argc, char *argv[])
 {
-  uint64_t *sprr = (uint64_t *)0xfffffc118;
+  uint64_t *sprr = (uint64_t *)0xfffffc110;
   printf("%llx %llx\n", sprr[0], sprr[1]);
+  uint64_t *sprr118 = (uint64_t *)0xfffffc118;
+  printf("%llx %llx\n", sprr118[0], sprr118[1]);
 }
