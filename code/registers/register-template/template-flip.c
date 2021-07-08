68574acd98433b2348950c7365f737373735a919
xss.cx <xss@xss.cx>
2021-07-08 10:59:51 -0400

    template cryptex os log add
    template cryptex os log add

--- code/registers/register-template/template-flip.c
+++ code/registers/register-template/template-flip.c
@@ -23,19 +23,19 @@ uint64_t read_register(void)
 }
 
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[]) { 
 {
-
-{
-    for (int j = 0; j < 64; ++j) {
-        printf("Read Initial Register S3_6_c15_c1_0 bit %02d: %016llx\n", j, read_register());
-    }
+    pid_t pid = getpid();
+	printf("register test from pid %d!\n", pid);
+    os_log_t log = os_log_create("com.example.cryptex.register-test", "run register template id");
+    os_log_info(log, "register from pid %d!", pid);
+	return 0;
 }
-
+{
 
     for (int i = 0; i < 64; ++i) {
         write_register(1ULL<<i);
         printf("Flipped Register S3_6_c15_c1_0 bit %02d: %016llx\n", i, read_register());
     }
 }
-
+}
--- code/registers/register-template/template-read.c
+++ code/registers/register-template/template-read.c
@@ -24,17 +24,18 @@ uint64_t read_sprr(void)
 
 int main(int argc, char *argv[]) {
 {
+    pid_t pid = getpid();
+	printf("register test from pid %d!\n", pid);
+    os_log_t log = os_log_create("com.example.cryptex.register-test", "run register template id");
+    os_log_info(log, "register from pid %d!", pid);
+	return 0;
+}
+
+{
     for (int i = 0; i < 64; ++i) {
 
         printf("s3_6_c15_c1_0 bit %02d: %016llx\n", i, read_sprr());
     }
 }
 
-{
-    pid_t pid = getpid();
-	printf("Hello researcher from pid %d!\n", pid);
-    os_log_t log = os_log_create("com.example.cryptex", "hello");
-    os_log_info(log, "Hello researcher from pid %d!", pid);
-	return 0;
-}
 }
