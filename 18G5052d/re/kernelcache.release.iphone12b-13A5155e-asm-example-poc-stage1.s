b3e3c43f6b79cab267a2d4913cfe4d95c51f8fb2
Hoyt LLC Research <xss@xss.cx>
2021-07-14 16:26:34 -0400

    Update kernelcache.release.iphone12b-13A5155e-asm-example-poc-stage1.s

--- 18G5052d/re/kernelcache.release.iphone12b-13A5155e-asm-example-poc-stage1.s
+++ 18G5052d/re/kernelcache.release.iphone12b-13A5155e-asm-example-poc-stage1.s
@@ -1,4 +1,9 @@
-	;xcrun -sdk iphoneos clang -g -O2  -mios-version-min=14.3 -DDEBUG=0  -Wall -Wpedantic -Wno-gnu -Werror -Wunused-variable -o z.out stage1.s
+;xcrun -sdk iphoneos clang -g -O2  -mios-version-min=14.3 -DDEBUG=0  -Wall -Wpedantic -Wno-gnu -Werror -Wunused-variable -o z.out stage1.s
+; Hand Rolled by David Hoyt | SRD0009 | @h02332 for Apple Security Research Device on Wednesday June 14, 2021
+; EDIT - This is an example.. you should recognize Code :-)
+; Your Mileage May Vary
+	;
+	;
 	.section	__TEXT,__text,regular,pure_instructions
 	.build_version ios, 15, 0	sdk_version 15, 0
 	.file	1 "/Users/xss/Downloads/awdl_exploit_release/shellcode" "poc-stage1.s"
