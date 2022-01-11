260fc3eb0b1b57f8341de17b3e66ec74edd567f3
David Hoyt <xss@xss.cx>
2022-01-11 11:00:21 -0500

    Update run-target-struct-sample-001.c

--- code/run-target-struct-sample-001.c
+++ code/run-target-struct-sample-001.c
@@ -3,6 +3,8 @@ static const struct runtime_target runtime_offset[] = {
     { "iPhone13,4", "18F72", rt_offset__iPhone13_4__18F72 },
     { "iPad8,12", "18F72", rt_offset__iPad8_12__18F72 },
     { "Mini9,11", "20F71", rt_offset__Mini9_12__20F71 },
-// duh    { "Macmini9,11", "20F71", rt_offset__Macmini9_12__20F71 },
+// duh    { "Macmini8,1", "20F71", rt_offset__Macmini9_12__20F71 },
+// duh    { "Macmini9,1", "20F71", rt_offset__Macmini9_12__20F71 },
     { "iPad8,6 20F71", "20F71", rt_offset__iPad8_6__20F71 },
+    { "MacBookPro17,1 20G314", "20G314", rt_offset__MacBookPro17__1__20G314 },
 };
