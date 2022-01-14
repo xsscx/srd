592b1f5c620a8bcbf4266c56152efe364cd84d17
David Hoyt <xss@xss.cx>
2022-01-14 11:06:46 -0500

    Update dmg.sh

--- SecurityResearchTools_21C39/example-cryptex/dmg.sh
+++ SecurityResearchTools_21C39/example-cryptex/dmg.sh
@@ -2,10 +2,8 @@
 mv srd-universal-cryptex.dmg srd-universal-cryptex.dmg.backup
 sudo cp src/toybox/toybox-src/generated/unstripped/toybox com.example.cryptex.dstroot/usr/bin
 codesign --force -s -  com.example.cryptex.dstroot/usr/bin/toybox
-hdiutil create -fs hfs+ -srcfolder com.example.cryptex.dstroot toyboxunstripped.dmg
-cryptexctl ${CRYPTEXCTL_FLAGS} create --replace ${CRYPTEXCTL_CREATE_FLAGS} -i com.example.cryptex -v 1.3.3.7 toyboxunstripped.dmg
+hdiutil create -fs hfs+ -srcfolder com.example.cryptex.dstroot srd-universal-cryptex.dmg
+cryptexctl ${CRYPTEXCTL_FLAGS} create --replace ${CRYPTEXCTL_CREATE_FLAGS} -i com.example.cryptex -v 1.3.3.7 srd-universal-cryptex.dmg
 cryptexctl uninstall com.example.cryptex
 cryptexctl install --variant=research --persist com.example.cryptex.cxbd.signed
 cryptexctl list
-mv toyboxunstripped.dmg srd-universal-cryptex.dmg
-
