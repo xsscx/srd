fa449857462e718a77530977042ab34a2d3b7d5c
David Hoyt <xss@xss.cx>
2022-01-14 08:08:15 -0500

    Updates to BEta
    Beta Updates

---
+++ beta-cryptex/src/welcome/welcome.c
@@ -0,0 +1,15 @@
+// Simple C program to display "Hello World"
+  
+// Header file for input output functions
+#include <stdio.h>
+  
+// main function -
+// where the execution of program begins
+int main()
+{
+  
+    // prints hello world
+    printf("Welcome to the Apple Security Research Device\n");
+  
+    return 0;
+}
