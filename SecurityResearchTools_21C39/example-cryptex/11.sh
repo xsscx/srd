f2c3504926e807a6636dc5c232d606f5f68900f3
David Hoyt <xss@xss.cx>
2022-01-16 15:30:29 -0500

    Update 11.sh

--- SecurityResearchTools_21C39/example-cryptex/11.sh
+++ SecurityResearchTools_21C39/example-cryptex/11.sh
@@ -42,7 +42,7 @@ sudo purge
 cryptexctl ${CRYPTEXCTL_FLAGS} create --research --replace ${CRYPTEXCTL_CREATE_FLAGS} --identifier=com.example.cryptex --version=1.3.3.7 --variant=research srd-universal-cryptex.dmg
 sudo purge
 echo "Personalize cryptex"
-cryptexctl ${CRYPTEXCTL_PERSONALIZE_FLAGS} personalize --replace  --variant=research com.example.cryptex.cxbd
+cryptexctl  personalize --replace -o /Users/xss/security-research-device-main/example-cryptex --variant=research /Users/xss/security-research-device-main/example-cryptex/com.example.cryptex.cxbd
 sudo purge
 echo "Install cryptex"
 cryptexctl install --variant=research --persist com.example.cryptex.cxbd.signed
