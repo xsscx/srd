6bf12e4719be3f30b5eb6912d0c88f1acb2b884a
David Hoyt <xss@xss.cx>
2022-01-16 09:03:08 -0500

    Update 11.sh

--- SecurityResearchTools_21C39/example-cryptex/11.sh
+++ SecurityResearchTools_21C39/example-cryptex/11.sh
@@ -1,6 +1,3 @@
-#!/bin/sh
-echo "Running Clean, Build, Install for iPhone 11"
-export CRYPTEXCTL_UDID=00008030-001538D03C40012E
 echo "DMG Backup"
 backup_files="srd-universal-cryptex.dmg"
 dest="/Users/xss/security-research-device-main/example-cryptex/dmg-backup"
@@ -26,20 +23,27 @@ echo "Running Purge"
 sudo purge
 echo "Copy Toybox Unstripped to the DST"
 sudo cp src/toybox/toybox-src/generated/unstripped/toybox com.example.cryptex.dstroot/usr/bin
+sudo purge
 echo "Codesign Ad Hoc"
 codesign --force -s -  com.example.cryptex.dstroot/usr/bin/toybox
+sudo purge
 echo "Delete the old DMG"
 rm srd-universal-cryptex.dmg
 echo "Create the Example DMG"
 hdiutil create -fs hfs+ -srcfolder com.example.cryptex.dstroot srd-universal-cryptex.dmg
+sudo purge
 echo "Uninstall existing com.example.cryptex"
 cryptexctl uninstall com.example.cryptex
 echo "Create the cryptex"
+sudo purge
 cryptexctl ${CRYPTEXCTL_FLAGS} create --research --replace ${CRYPTEXCTL_CREATE_FLAGS} --identifier=com.example.cryptex --version=1.3.3.7 --variant=research srd-universal-cryptex.dmg
+sudo purge
 echo "Personalize cryptex"
 cryptexctl ${CRYPTEXCTL_PERSONALIZE_FLAGS} personalize --replace  --variant=research com.example.cryptex.cxbd
+sudo purge
 echo "Install cryptex"
 cryptexctl install --variant=research --persist com.example.cryptex.cxbd.signed
 echo "Check Installation"
 cryptexctl list
+sudo purge
 echo "Done"
