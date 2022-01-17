784595ba7811c36a80bde836bc646200cf85e57f
David Hoyt <xss@xss.cx>
2022-01-17 09:51:13 -0500

    Update srd-cryptex-troubleshooter.sh

--- SecurityResearchTools_21C39/example-cryptex/srd-cryptex-troubleshooter.sh
+++ SecurityResearchTools_21C39/example-cryptex/srd-cryptex-troubleshooter.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 echo "SRD Cryptex Troubleshooter Log Collector Start"
+echo "Rotate Log"
+mv srd-cryptex-troubleshooter.log srd-cryptex-troubleshooter.log.bak
 echo "umount com.example.cryptex"
 cryptexctl uninstall com.example.cryptex
-date > srd-cryptex-troubleshooter.log
-uname -a >> srd-cryptex-troubleshooter.log
-cryptexctl version >> srd-cryptex-troubleshooter.log
-sysctl -a | grep brand >> srd-cryptex-troubleshooter.log
-csrutil status >> srd-cryptex-troubleshooter.log
-cryptexctl -v2 -d2  install --variant=research --persist --print-info ./com.example.cryptex.cxbd.signed
+date > srd-cryptex-troubleshooter.log 2>&1
+uname -a >> srd-cryptex-troubleshooter.log 2>&1
+cryptexctl version >> srd-cryptex-troubleshooter.log 2>&1
+sysctl -a | grep brand >> srd-cryptex-troubleshooter.log 2>&1
+csrutil status >> srd-cryptex-troubleshooter.log 2>&1
+cryptexctl -v2 -d2  install --variant=research --persist --print-info ./com.example.cryptex.cxbd.signed >> srd-cryptex-troubleshooter.log 2>&1
 echo "Check for cryptex install"
-cryptexctl list
+cryptexctl list 2>&1
 clang -v >> srd-cryptex-troubleshooter.log 2>&1
 # sudo sysdiagnose
+echo "SRD Cryptex Troubleshooter Logs"
 cat srd-cryptex-troubleshooter.log
 echo "SRD Cryptex Troubleshooter Log Collector End"
