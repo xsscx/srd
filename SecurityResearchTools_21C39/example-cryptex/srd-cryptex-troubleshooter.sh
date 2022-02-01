e5201da6c64c4bcde24ac612ce432ae06c4a7c32
David Hoyt <xss@xss.cx>
2022-02-01 16:01:49 -0500

    Update srd-cryptex-troubleshooter.sh

--- SecurityResearchTools_21C39/example-cryptex/srd-cryptex-troubleshooter.sh
+++ SecurityResearchTools_21C39/example-cryptex/srd-cryptex-troubleshooter.sh
@@ -8,6 +8,7 @@ echo "Collecting Info"
 date > srd-cryptex-troubleshooter.log 2>&1
 uname -a >> srd-cryptex-troubleshooter.log 2>&1
 clang -v >> srd-cryptex-troubleshooter.log 2>&1
+xcodebuild -showsdks  >> srd-cryptex-troubleshooter.log 2>&1
 cryptexctl version >> srd-cryptex-troubleshooter.log 2>&1
 sysctl -a | grep brand >> srd-cryptex-troubleshooter.log 2>&1
 csrutil status >> srd-cryptex-troubleshooter.log 2>&1
@@ -20,4 +21,6 @@ echo "====================================="
 echo "SRD Cryptex Troubleshooter Log Info"
 echo "====================================="
 cat srd-cryptex-troubleshooter.log
+echo "Check for cryptex install on SRD"
+cryptexctl list
 echo "SRD Cryptex Log Collector Stop"
