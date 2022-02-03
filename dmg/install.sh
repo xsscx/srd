f6aa162a006a02930ba840ba97eec53458273f8f
David Hoyt <xss@xss.cx>
2022-02-03 10:37:21 -0500

    Update install.sh

--- dmg/install.sh
+++ dmg/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 wget https://xss.cx/srd/dmg/srd-universal-cryptex.dmg
 cryptexctl ${CRYPTEXCTL_FLAGS} create --research --replace ${CRYPTEXCTL_CREATE_FLAGS} --identifier=com.example.cryptex --version=1.3.3.7 --variant=research srd-universal-cryptex.dmg
-cryptexctl  personalize --replace -o example-cryptex --variant=research example-cryptex/com.example.cryptex.cxbd
+cryptexctl ${CRYPTEXCTL_PERSONALIZE_FLAGS} personalize --replace  --variant=research com.example.cryptex.cxbd
 cryptexctl uninstall com.example.cryptex
 cryptexctl install --variant=research --persist com.example.cryptex.cxbd.signed
 cryptexctl list
