14965ce5f62388da493818224dbe4db332a422c2
David Hoyt <xss@xss.cx>
2022-02-03 10:47:02 -0500

    Update build.sh

--- SecurityResearchTools_21C39/example-cryptex/build.sh
+++ SecurityResearchTools_21C39/example-cryptex/build.sh
@@ -4,21 +4,15 @@ sudo purge
 make clean
 sudo purge
 make
-sudo purge
 codesign -s - --entitlements src/toybox/entitlements.plist  src/toybox/toybox-src/generated/unstripped/toybox
 sudo cp src/toybox/toybox-src/generated/unstripped/toybox com.example.cryptex.dstroot/usr/bin
 sudo purge
 codesign --force -s -  com.example.cryptex.dstroot/usr/bin/toybox
 sudo purge
 hdiutil create -fs hfs+ -srcfolder com.example.cryptex.dstroot srd-universal-cryptex.dmg
-sudo purge
-cryptexctl ${CRYPTEXCTL_FLAGS} create --research --replace ${CRYPTEXCTL_CREATE_FLAGS} --identifier=com.example.cryptex --version=1.3.3.7 --variant=research srd-universal-cryptex.dmg
-sudo purge
-cryptexctl  personalize --replace -o /Users/xss/security-research-device-main/example-cryptex --variant=research /Users/xss/security-research-device-main/example-cryptex/com.example.cryptex.cxbd
-sudo purge
+cryptexctl ${CRYPTEXCTL_FLAGS} create --research --replace ${CRYPTEXCTL_CREATE_FLAGS} --identifier=com.example.cryptex --version=1.3.3.7 --variant=research toyboxunstripped.dmg
+cryptexctl ${CRYPTEXCTL_PERSONALIZE_FLAGS} personalize --replace  --variant=research com.example.cryptex.cxbd
 cryptexctl uninstall com.example.cryptex
-sudo purge
 cryptexctl install --variant=research --persist com.example.cryptex.cxbd.signed
-sudo purge
 cryptexctl list
 sudo purge
