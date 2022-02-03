574004567a1d8beab8ebeeecbf251b5a0d087a2a
David Hoyt <xss@xss.cx>
2022-02-03 10:44:06 -0500

    Update install.sh

--- dmg/install.sh
+++ dmg/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 wget https://xss.cx/srd/dmg/srd-universal-cryptex.dmg
-cryptexctl ${CRYPTEXCTL_FLAGS} create --research --replace ${CRYPTEXCTL_CREATE_FLAGS} --identifier=com.example.cryptex --version=1.3.3.7 --variant=research toyboxunstripped.dmg
+cryptexctl ${CRYPTEXCTL_FLAGS} create --research --replace ${CRYPTEXCTL_CREATE_FLAGS} --identifier=com.example.cryptex --version=1.3.3.7 --variant=research srd-universal-cryptex.dmg
 cryptexctl ${CRYPTEXCTL_PERSONALIZE_FLAGS} personalize --replace  --variant=research com.example.cryptex.cxbd
 cryptexctl uninstall com.example.cryptex
 cryptexctl install --variant=research --persist com.example.cryptex.cxbd.signed
