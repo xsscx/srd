84554e677bbe00cd6f0d0bdbcc2b80f073a4fc39
David Hoyt <xss@xss.cx>
2022-02-13 10:41:22 -0500

    Build Script with Initial Unit Testing
    Build Script with Initial Unit Testing

--- srd_tools-24.100.3/example-cryptex/build.sh
+++ srd_tools-24.100.3/example-cryptex/build.sh
@@ -1,17 +1,60 @@
 #!/bin/sh
-rm -rf srd-universal-cryptex.dmg
+echo "Start the Build"
 make clean
-make
+make all
+echo "Start of entitlement checks..... for example-cryptex with debugserver and latest entitlements from PR48 + PR49....."
+rm /private/tmp/*.xml
+echo "Check the entitlements in the src/"
+codesign --display --entitlements - --xml src/frida/frida-agent.dylib > /private/tmp/src-frida-agent.xml
+codesign --display --entitlements - --xml src/frida/frida-server > /private/tmp/src-frida-server.xml
+codesign --display --entitlements - --xml src/dropbear/dropbear > /private/tmp/src-dropbear.xml
+echo "Changing to toybox unstripped"
 chmod 775 src/toybox/toybox-src/generated/unstripped/toybox
-codesign -s - --entitlements src/toybox/entitlements.plist  src/toybox/toybox-src/generated/unstripped/toybox
+codesign --force -s - --entitlements src/toybox/entitlements.plist  src/toybox/toybox-src/generated/unstripped/toybox
 sudo cp src/toybox/toybox-src/generated/unstripped/toybox com.example.cryptex.dstroot/usr/bin
-sudo purge
 codesign --force -s -  com.example.cryptex.dstroot/usr/bin/toybox
-sudo purge
+codesign --force -s - --entitlements src/toybox/entitlements.plist com.example.cryptex.dstroot/usr/bin/toybox
+codesign --display --entitlements - --xml src/toybox/toybox-src/generated/unstripped/toybox > /private/tmp/src-toybox.xml
+codesign --display --entitlements - --xml src/simple-shell/simple-shell > /private/tmp/src-simple-server.xml
+codesign --display --entitlements - --xml src/debugserver/debugserver > /private/tmp/src-debugserver.xml
+codesign --display --entitlements - --xml src/hello/hello > /private/tmp/src-hello.xml
+codesign --display --entitlements - --xml src/simple-server/simple-server > /private/tmp/src-simple-server.xml
+codesign --display --entitlements - --xml src/nvram/nvram > /private/tmp/src-nvram.xml
+codesign --display --entitlements - --xml src/cryptex-run/cryptex-run > /private/tmp/src-cryptex-run.xml
+echo "Check the entitlements in the dstroot/"
+codesign --display --entitlements - --xml com.example.cryptex.dstroot/usr/lib/frida/frida-agent.dylib > /private/tmp/dst-frida-agent.xml
+codesign --display --entitlements - --xml com.example.cryptex.dstroot/usr/bin/frida-server > /private/tmp/dst-frida-server.xml
+codesign --display --entitlements - --xml com.example.cryptex.dstroot/usr/bin/dropbear > /private/tmp/dst-dropbear.xml
+codesign --display --entitlements - --xml com.example.cryptex.dstroot/usr/bin/toybox > /private/tmp/dst-toybox.xml
+codesign --display --entitlements - --xml com.example.cryptex.dstroot/usr/bin/simple-shell > /private/tmp/dst-simple-server.xml
+codesign --display --entitlements - --xml com.example.cryptex.dstroot/usr/bin/debugserver > /private/tmp/dst-debugserver.xml
+codesign --display --entitlements - --xml com.example.cryptex.dstroot/usr/bin/hello > /private/tmp/dst-hello.xml
+codesign --display --entitlements - --xml com.example.cryptex.dstroot/usr/bin/simple-server > /private/tmp/dst-simple-server.xml
+codesign --display --entitlements - --xml com.example.cryptex.dstroot/usr/bin/nvram > /private/tmp/dst-nvram.xml
+codesign --display --entitlements - --xml com.example.cryptex.dstroot/usr/bin/cryptex-run > /private/tmp/dst-cryptex-run.xml
+echo "diff the entitlements... if anything different check Console Log.. cryptex install has failed if the entitlements aren't the same.."
+echo "Check for frida-agent"
+diff /private/tmp/src-frida-agent.xml /private/tmp/dst-frida-agent.xml
+echo "Check for frida-server"
+diff /private/tmp/src-frida-server.xml /private/tmp/dst-frida-server.xml
+echo "Check for dropbear"
+diff /private/tmp/src-dropbear.xml /private/tmp/dst-dropbear.xml
+echo "Check for toybox"
+diff /private/tmp/src-toybox.xml /private/tmp/dst-toybox.xml
+echo "Check for simple-server"
+diff /private/tmp/src-simple-server.xml /private/tmp/dst-simple-server.xml
+echo "Check for debugserver"
+diff /private/tmp/src-debugserver.xml /private/tmp/dst-debugserver.xml
+echo "Check for hello"
+diff /private/tmp/src-hello.xml /private/tmp/dst-hello.xml
+echo "Check for nvram"
+diff /private/tmp/src-nvram.xml /private/tmp/dst-nvram.xml
+echo "Check for cryptex-run"
+diff /private/tmp/src-cryptex-run.xml /private/tmp/dst-cryptex-run.xml
+echo "End of entitlement checks....."
 hdiutil create -fs hfs+ -srcfolder com.example.cryptex.dstroot srd-universal-cryptex.dmg
 cryptexctl ${CRYPTEXCTL_FLAGS} create --research --replace ${CRYPTEXCTL_CREATE_FLAGS} --identifier=com.example.cryptex --version=1.3.3.7 --variant=research srd-universal-cryptex.dmg
 cryptexctl ${CRYPTEXCTL_PERSONALIZE_FLAGS} personalize --replace  --variant=research com.example.cryptex.cxbd
 cryptexctl uninstall com.example.cryptex
 cryptexctl install --variant=research --persist com.example.cryptex.cxbd.signed
 cryptexctl list
-sudo purge
