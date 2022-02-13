1d85d30e815c5d137755ddaccd8cd405de1d70bd
David Hoyt <xss@xss.cx>
2022-02-13 10:48:14 -0500

    Update example-cryptex-ubsan-entitlement-checks.sh

--- srd_tools-24.100.3/example-cryptex/example-cryptex-ubsan-entitlement-checks.sh
+++ srd_tools-24.100.3/example-cryptex/example-cryptex-ubsan-entitlement-checks.sh
@@ -57,3 +57,9 @@ diff /private/tmp/src-cryptex-run.xml /private/tmp/dst-cryptex-run.xml
 echo "Check for libclang_rt.ubsan_ios_dynamic.dylib"
 diff /private/tmp/src-libclang_rt.ubsan_ios_dynamic.dylib.xml /private/tmp/dst-libclang_rt.ubsan_ios_dynamic.dylib.xml
 echo "End of entitlement checks....."
+hdiutil create -fs hfs+ -srcfolder com.example.cryptex.dstroot srd-universal-cryptex.dmg
+cryptexctl ${CRYPTEXCTL_FLAGS} create --research --replace ${CRYPTEXCTL_CREATE_FLAGS} --identifier=com.example.cryptex --version=1.3.3.7 --variant=research srd-universal-cryptex.dmg
+cryptexctl ${CRYPTEXCTL_PERSONALIZE_FLAGS} personalize --replace  --variant=research com.example.cryptex.cxbd
+cryptexctl uninstall com.example.cryptex
+cryptexctl install --variant=research --persist com.example.cryptex.cxbd.signed
+cryptexctl list
