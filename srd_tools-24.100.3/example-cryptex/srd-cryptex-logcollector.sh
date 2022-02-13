0fff3b765bf743696f988a075fb2f35e15e3320d
David Hoyt <xss@xss.cx>
2022-02-13 06:53:53 -0500

    Add files via upload

---
+++ srd_tools-24.100.3/example-cryptex/srd-cryptex-logcollector.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+echo "================================="
+echo "SRD Cryptex Log Collector Start"
+echo "================================="
+echo "umount com.example.cryptex"
+cryptexctl uninstall com.example.cryptex
+echo "Collecting Info"
+date > srd-cryptex-troubleshooter.log 2>&1
+sysctl -a | grep kern.version >> srd-cryptex-troubleshooter.log 2>&1
+sysctl -a | grep kern.osversion >> srd-cryptex-troubleshooter.log 2>&1
+sysctl -a | grep kern.iossupportversion >> srd-cryptex-troubleshooter.log 2>&1
+sysctl -a | grep kern.osproductversion >> srd-cryptex-troubleshooter.log 2>&1
+sysctl -a | grep kern.osproductversioncompat >> srd-cryptex-troubleshooter.log 2>&1
+xcrun  --show-sdk-path  >> srd-cryptex-troubleshooter.log 2>&1
+cryptexctl device list >> srd-cryptex-troubleshooter.log 2>&1
+clang -v >> srd-cryptex-troubleshooter.log 2>&1
+cryptexctl version >> srd-cryptex-troubleshooter.log 2>&1
+sysctl -a | grep brand >> srd-cryptex-troubleshooter.log 2>&1
+csrutil status >> srd-cryptex-troubleshooter.log 2>&1
+echo "Install cryptex to SRD"
+cryptexctl -v9 -d9 -ldt  install --variant=research --persist --print-info ./com.example.cryptex.cxbd.signed >> srd-cryptex-troubleshooter.log 2>&1
+echo "====================================="
+echo "Check for cryptex install on SRD"
+cryptexctl list 2>&1
+echo "====================================="
+echo "SRD Cryptex Troubleshooter Log Info"
+echo "====================================="
+cat srd-cryptex-troubleshooter.log
+echo "Check for cryptex install on SRD"
+cryptexctl list
+echo "SRD Cryptex Log Collector Stop"
+
